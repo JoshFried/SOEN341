dc6246c01d98182869bdecd8596fe119450038e6
Sacha Elkaim <sachaelkaim87@gmail.com>
2020-02-13 04:50:55 -0500

    setup of frontend
    general setup of api/routingPages/layout/bootstrap, upload and display an img for the front end

---
+++ front-end/instagram/src/components/UserProfile/followers.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Followers = props => {
+  return (
+    <div>
+      <h2 className="followers">followers</h2>
+      <h2 className="followersNumber">878</h2>
+    </div>
+  );
+};
+
+export default Followers;
---
+++ front-end/instagram/src/components/UserProfile/username.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Username = props => {
+  return <h1 className="username">username</h1>;
+};
+
+export default Username;
