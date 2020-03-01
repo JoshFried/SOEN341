c0160fadccd6fae3a920e3df7dcbe5b44b52b716
Sacha Elkaim <sachaelkaim87@gmail.com>
2020-03-01 04:35:25 -0500

    removeVe

--- back-end/insta/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sacha\documents\github\soen341\back-end\insta\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
