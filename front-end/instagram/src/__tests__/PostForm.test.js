d17adb6def7df41ebc4fb757f87609e10cb2691f
Sacha Elkaim <sachaelkaim87@gmail.com>
2020-04-07 03:54:40 -0400

    final fixes
    make sure axios post call to API  is called with the right data and that everything is working correctly(input fields, handle methods, submit call).

--- front-end/instagram/src/__tests__/PostForm.test.js
+++ front-end/instagram/src/__tests__/PostForm.test.js
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "../App";
 import PostForm from "../components/Postform.js";
 import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
@@ -8,15 +6,15 @@ import Adapter from 'enzyme-adapter-react-16';
 import axios from 'axios';
 
 Enzyme.configure({ adapter: new Adapter() });
-jest.mock("axios");
 global.URL.createObjectURL = jest.fn();
+
 /*UPLOAD FORM TEST*/
 describe('PostForm', () => {
   it("renders", () => {
     shallow(<PostForm />);
   });
-  it("Fill form, submit and get a response from API post method", () => {
-    axios.post.mockImplementation(() => Promise.resolve({ message: 'uploaded'}))
+  it("Fill form, submit and verify post method call to API", () => {
+    jest.spyOn(axios, 'post').mockImplementation(() => Promise.resolve({ message: 'uploaded'}))
     const wrapper = shallow(<PostForm />)
     let captionInput = wrapper.find('#content')
     captionInput.simulate('change', {
@@ -31,15 +29,16 @@ describe('PostForm', () => {
         files: [file]
       }
     })
+    let form_data = new FormData();
+    form_data.append("picture", file, file.name);
+    form_data.append("caption", captionInput.props().value);
+    form_data.append("img_filter", '');
     const mockFn = jest.fn( localStorage.setItem )
     localStorage.setItem = mockFn
     localStorage.setItem( 'token', 'a229238812312')
     wrapper.find('form').at(0).simulate('submit', {
     preventDefault: () => {},
     })
-    /* Our image file and caption are in the right format and working correctly or else formData in Postform would through an error
-    after clicking submit and going into handleSubmit().
-    */
-    expect(axios.post).toHaveBeenCalled()
+    expect(axios.post).toHaveBeenCalledWith("http://127.0.0.1:8000/api/post/create", form_data, {"headers": {"Authorization": "Token a229238812312", "content-type": "multipart/form-data"}})
   })
 })
