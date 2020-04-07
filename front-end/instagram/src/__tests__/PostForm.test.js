ba0bea40c8f56c54a90d0d47b391efa6613d508e
Sacha Elkaim <sachaelkaim87@gmail.com>
2020-04-07 03:11:05 -0400

    upload-form-unit-test
    form test using enzyme/jest

---
+++ front-end/instagram/src/__tests__/PostForm.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+import PostForm from "../components/Postform.js";
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import axios from 'axios';
+
+Enzyme.configure({ adapter: new Adapter() });
+jest.mock("axios");
+global.URL.createObjectURL = jest.fn();
+/*UPLOAD FORM TEST*/
+describe('PostForm', () => {
+  it("renders", () => {
+    shallow(<PostForm />);
+  });
+  it("Fill form, submit and get a response from API post method", () => {
+    axios.post.mockImplementation(() => Promise.resolve({ message: 'uploaded'}))
+    const wrapper = shallow(<PostForm />)
+    let captionInput = wrapper.find('#content')
+    captionInput.simulate('change', {
+      target: { value: 'this is a test caption'}
+    })
+    captionInput = wrapper.find('#content')
+    expect(captionInput.props().value).toEqual('this is a test caption')
+    let uploadPhoto = wrapper.find('#myInput')
+    const file = new File(['test'], 'test.jpg', { type: 'image/png' })
+    uploadPhoto.simulate('change', {
+      target: {
+        files: [file]
+      }
+    })
+    const mockFn = jest.fn( localStorage.setItem )
+    localStorage.setItem = mockFn
+    localStorage.setItem( 'token', 'a229238812312')
+    wrapper.find('form').at(0).simulate('submit', {
+    preventDefault: () => {},
+    })
+    expect(axios.post).toHaveBeenCalled()
+  })
+})
